0c4f5595c1566b73de7131e2096b4a54cc1eb466
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-02-16 23:58:43 +0200

    Navigation graph added and dependency for project

---
+++ app/src/main/java/com/seif/eshraqaapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.seif.eshraqaapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.util.*
+import kotlin.collections.HashMap
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
